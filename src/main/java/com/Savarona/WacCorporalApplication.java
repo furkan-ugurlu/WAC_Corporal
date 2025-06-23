f29df9263f6448cd332b22db31e5eecd76756b7b
Furkan Uğurlu <furkanugurlu@192.168.1.10>
2025-06-24 01:47:29 +0300

    Initial commit: WAC Corporal

---
+++ src/main/java/com/Savarona/WacCorporalApplication.java
@@ -0,0 +1,14 @@
+package com.Savarona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WacCorporalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WacCorporalApplication.class, args);
+	}
+
+}
